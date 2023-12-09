8d02042e3bddecbf0bdc31205132aceac14e6f23
lI15SO0 <svvivi@outlook.com>
2023-12-09 23:36:36 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rime 配置文件
+
+---
+
+## 关于
+
+这个是我个人使用的配置文件，使用的主要是 98版五笔 作为输入方案
+
+根据(这个仓库)[https://github.com/arzyu/rime-wubi98]修改而来
+
+码表来自于(这里仓库)[https://github.com/omgredfog/98wubi]
+
+---
+
+如果仅使用码表，复制 以下文件
+
+```
+wubi98.dict.yaml
+wubi98.schema.yaml
+pinyin_simp.dict.yaml
+pinyin_simp.schema.yaml
+rime.lua
+```
---
+++ rime.lua
@@ -0,0 +1,35 @@
+function date_translator(input, seg)
+    if (input == "datetime") then
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), ""))
+    end
+
+    if (input == "date") then
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+    end
+
+    if (input == "time") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+    end
+
+    if (input == "week") then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%A"), ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%a"), "缩写"))
+    end
+
+    if (input == "month") then
+        yield(Candidate("month", seg.start, seg._end, os.date("%B"), ""))
+        yield(Candidate("month", seg.start, seg._end, os.date("%b"), "缩写"))
+    end
+end
+
+
